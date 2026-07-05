833429ea22dd2509f62a7fcb01e60cfed0d59d25
Brant Monaghan <59965015+b24177@users.noreply.github.com>
2026-07-05 15:38:07 +0200

    Add basic smoke and model tests

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'is invalid without required associations' do
+    notification = Notification.new
+
+    assert_not notification.valid?
+    assert_includes notification.errors.attribute_names, :user
+    assert_includes notification.errors.attribute_names, :post
+  end
 end
